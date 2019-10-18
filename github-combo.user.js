33a1fa6b11015ff14c4cff80793e51422d624934
Haitao <liubiantao@users.noreply.github.com>
2019-10-18 19:34:00 +0800

    Update github-combo.user.js

--- github-combo.user.js
+++ github-combo.user.js
@@ -1,50 +1,66 @@
-const starButton = document.querySelectorAll(
-  'button.btn.btn-sm.btn-with-count.js-toggler-target'
-)[1]
-const watchButton = document.querySelector('button[value=release_only]')
-const forkButton = document.querySelector('.octicon-repo-forked').parentElement
+// ==UserScript==
+// @name         Github 素质三连
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Github 长按 star, 一键 watch, star, fork
+// @author       liubiantao
+// @match        https://*.github.com/*
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
 
-const getTimeNow = () => new Date().getTime()
+;(function() {
+  'use strict'
 
-let timeStart, timeEnd, time
-let playing = false
+  const starButton = document.querySelectorAll(
+    'button.btn.btn-sm.btn-with-count.js-toggler-target'
+  )[1]
+  const watchButton = document.querySelector('button[value=release_only]')
+  const forkButton = document.querySelector('.octicon-repo-forked')
+    .parentElement
 
-function playAnimation() {
-  playing = true
-  console.warn('playAnimation')
-}
+  const getTimeNow = () => new Date().getTime()
 
-function stopAnimation() {
-  playing = false
-  console.warn('stopAnimation')
-}
+  let timeStart, timeEnd, time
+  let playing = false
 
-function combo() {
-  starButton.click()
-  watchButton.click()
-  forkButton.click()
-}
+  function playAnimation() {
+    playing = true
+    console.warn('playAnimation')
+  }
 
-function holdDown() {
-  timeStart = getTimeNow()
-  time = setInterval(function() {
-    timeEnd = getTimeNow()
+  function stopAnimation() {
+    playing = false
+    console.warn('stopAnimation')
+  }
 
-    if (timeEnd - timeStart > 200) {
-      !playing && playAnimation()
-    }
+  function combo() {
+    starButton.click()
+    watchButton.click()
+    forkButton.click()
+  }
 
-    if (timeEnd - timeStart > 1500) {
-      clearInterval(time)
-      combo()
-    }
-  }, 100)
-}
+  function holdDown() {
+    timeStart = getTimeNow()
+    time = setInterval(function() {
+      timeEnd = getTimeNow()
 
-function holdUp() {
-  clearInterval(time)
-  playing && stopAnimation()
-}
+      if (timeEnd - timeStart > 200) {
+        !playing && playAnimation()
+      }
 
-starButton.addEventListener('mousedown', holdDown)
-starButton.addEventListener('mouseup', holdUp)
+      if (timeEnd - timeStart > 1500) {
+        clearInterval(time)
+        combo()
+      }
+    }, 100)
+  }
+
+  function holdUp() {
+    clearInterval(time)
+    playing && stopAnimation()
+  }
+
+  starButton.addEventListener('mousedown', holdDown)
+  starButton.addEventListener('mouseup', holdUp)
+})()
